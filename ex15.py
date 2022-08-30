233f065b05e3f251f6ff2fc22bd513647b611561
Ariane <aciattei@gmail.com>
2022-08-30 19:37:59 -0300

    perf

--- ex15.py
+++ ex15.py
@@ -3,8 +3,8 @@
 # No programa eles informam o valor da conta de energia e quantos que irão pagar a conta no mês. 
 # O programa calculará quanto cada um deverá contribuir com a conta de energia.
 
-valor_da_conta = float(input())
-quantidade_de_pessoas = int(input())
+valor_da_conta = float(input('Informe o valor da conta: '))
+quantidade_de_pessoas = int(input('Informe a quantidade de moradores: '))
 
 valor_por_pessoa = valor_da_conta/quantidade_de_pessoas
-print(valor_por_pessoa)
+print(f'O valor a ser pago por cada morador é de R$ {valor_por_pessoa}')
