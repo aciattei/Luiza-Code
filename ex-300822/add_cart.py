34399ad2928f512fa2669ef59b894f58c0f08245
Ariane <aciattei@gmail.com>
2022-09-02 18:55:32 -0300

    fix

--- ex-300822/add_cart.py
+++ ex-300822/add_cart.py
@@ -7,13 +7,10 @@ def add_item_cart(item, cart):
   cart.append(item)
   return cart
 
-def execute():
-  id_user = input('Insira o id do usuário: ')
-  id_product = input('Insira o id do produto: ')
-  price_product = float(input('Insira o preço do produto: '))
-  quantity_product = int(input('Insira a quantidade do produto: '))
-  item = [id_user, id_product, price_product, quantity_product]
-  cart = add_item_cart(item, cart)
-  print(cart)
-
-execute()
+id_user = input('Insira o id do usuário: ')
+id_product = input('Insira o id do produto: ')
+price_product = float(input('Insira o preço do produto: '))
+quantity_product = int(input('Insira a quantidade do produto: '))
+item = [id_user, id_product, price_product, quantity_product]
+cart = add_item_cart(item, cart)
+  
