b5a5cb2d54b8c6d09088f236f1ef136d15b75cee
Ariane <aciattei@gmail.com>
2022-09-02 14:24:38 -0300

    teste

--- ex-300822/add_cart.py
+++ ex-300822/add_cart.py
@@ -1,10 +1,19 @@
+#Criar função para adicionar item ao carrinho
+#cart
 cart = []
-cart_total = 0
 
-def add_cart(item_id, item_price):
-  cart.append(item_id)
-  return item_price
-cart_total += add_cart('Apple', 100)
+def add_item_cart(item, cart):
+  #adicionar a lista intem à lista cart
+  cart.append(item)
+  return cart
 
-print(cart)
-print(cart_total)
+def execute():
+  id_user = input('Insira o id do usuário: ')
+  id_product = input('Insira o id do produto: ')
+  price_product = float(input('Insira o preço do produto: '))
+  quantity_product = int(input('Insira a quantidade do produto: '))
+  item = [id_user, id_product, price_product, quantity_product]
+  cart = add_item_cart(item, cart)
+  print(cart)
+
+execute()
