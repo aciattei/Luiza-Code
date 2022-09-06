b3a45ff06582d9d58b6952f73df6f558c154f568
Ariane <aciattei@gmail.com>
2022-09-06 20:49:24 -0300

    fix

--- ex-300822/add_cart.py
+++ ex-300822/add_cart.py
@@ -1,16 +1,19 @@
-#Criar função para adicionar item ao carrinho
-#cart
 cart = []
-
-def add_item_cart(item, cart):
-  #adicionar a lista intem à lista cart
-  cart.append(item)
-  return cart
 
 id_user = input('Insira o id do usuário: ')
 id_product = input('Insira o id do produto: ')
 price_product = float(input('Insira o preço do produto: '))
 quantity_product = int(input('Insira a quantidade do produto: '))
+
 item = [id_user, id_product, price_product, quantity_product]
-cart = add_item_cart(item, cart)
+#id_user, id_product, price_product, quantity_product
+def add_item_cart():
+  cart.append(item)
+  return cart
   
+def get_all_items_cart():
+  return cart
+
+def get_item_cart_by_id(id_product):
+  new_list = [item for item in cart if item[0 == id_product]]
+  return new_list
